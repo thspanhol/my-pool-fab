b058bde0f80d410d08ce02dd79d1ca7d23bc659a
thales <thales_spanhol@hotmail.com>
2024-11-25 12:11:37 -0300

    add cardsControllerTest

--- src/main/java/my/pool/api/model/Pool.java
+++ src/main/java/my/pool/api/model/Pool.java
@@ -1,6 +1,7 @@
 package my.pool.api.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Document(collection = "pool")
 public class Pool {
 
