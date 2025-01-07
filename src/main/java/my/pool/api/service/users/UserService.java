310552961316e5e7e8f2a6269ca449678f10299c
thales <thales_spanhol@hotmail.com>
2025-01-07 10:20:24 -0300

    fix editUser and deleteUser

--- src/main/java/my/pool/api/service/users/UserService.java
+++ src/main/java/my/pool/api/service/users/UserService.java
@@ -1,37 +1,47 @@
 package my.pool.api.service.users;
 
 import lombok.RequiredArgsConstructor;
+import my.pool.api.repository.PoolRepository;
 import my.pool.api.service.users.models.UserDTO;
 import my.pool.api.service.users.models.UserEntity;
 import my.pool.api.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PoolRepository poolRepository;
 
-      public Mono<UserEntity> find(String userId) {
-          return userRepository.findById(userId)
-                  .switchIfEmpty(Mono.error(new RuntimeException("User not found.")));
-      }
+    public Mono<UserEntity> find(String userId) {
+        return userRepository.findById(userId)
+                .switchIfEmpty(Mono.error(new RuntimeException("User not found.")));
+    }
 
     public Mono<Void> create(UserDTO userDTO) {
         return userRepository.insert(new UserEntity(userDTO))
-                .then(); // Retorna um Mono<Void> após a inserção
+                .then();
     }
 
     public Mono<Void> edit(String userId, UserDTO userDTO) {
         return userRepository.findById(userId)
+                .switchIfEmpty(Mono.error(new RuntimeException("User not found.")))
                 .flatMap(user -> userRepository.save(userDTO.retornaUser(user)))
-                .then(); // Retorna um Mono<Void> após a atualização
+                .then();
     }
 
     public Mono<Void> delete(String userId) {
         return userRepository.findById(userId)
                 .switchIfEmpty(Mono.error(new RuntimeException("User not found.")))
-                .flatMap(user -> userRepository.deleteById(user.getId()));
+                .flatMap(user -> Flux.fromIterable(user.getPools())
+                        .flatMap(poolRepository::deleteById)
+                        .then(userRepository.deleteById(user.getId())));
     }
+
 }
