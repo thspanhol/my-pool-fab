6ebc9c7befeb6a4171bcabd9d3b0898ff1a28053
thales <thales_spanhol@hotmail.com>
2025-01-09 09:38:59 -0300

    implement spring cloud config

--- src/main/java/my/pool/api/integration/models/Card.java
+++ src/main/java/my/pool/api/integration/models/Card.java
@@ -3,8 +3,10 @@ package my.pool.api.integration.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Card {
